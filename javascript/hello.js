a086ad3207480eaa7687ec3a4b70726c43102e17
chinxx5220 <chinmaypanchannavar2003@gmail.com>
2024-11-30 11:32:46 +0530

    Add files via upload

---
+++ javascript/hello.js
@@ -0,0 +1,6 @@
+var name= "Chinmay panchannavar";
+console.log(name);
+let age=21;
+const isStudent= true;
+
+console.log(name,age,isStudent)
---
+++ javascript/variable.js
@@ -0,0 +1,8 @@
+let num1= 300;
+let num2= 200;
+let result= num1*num2;
+
+document.getElementById('output').innerHTML = `
+    <p>Number 1:${num1}</p>
+    <p>Number 2:${num2}</p>
+    <p>multiplication: ${result}</p>`;
